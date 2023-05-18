71b305c8e7edea6449fd01a4273c281b1e7ab14b
Cristian Gutierrez <cristian07199420@gmail.com>
2023-05-17 22:12:10 -0500

    arreglo del header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,15 +9,15 @@ const NavBar = () => {
     }
 
   return (
-    <header className="flex justify-between py-8 px-5 text-2xl text-white items-center bg-[#1A0B2E] relative md:py-3">
+    <header className="flex justify-between py-8 px-5 text-2xl text-white items-center bg-[#1A0B2E] relative md:py-0">
       <h1 className="font-semibold">Cristian.G</h1>
       <i onClick={handleShowNav} className="bx bx-menu text-3xl cursor-pointer md:hidden"></i>
 
-      <nav className={`absolute top-full ${isShowNav ? "left-0" : "-left-full"} w-full grid text-lg px-2 bg-gray-600 duration-200 md:static md:flex md:w-auto md:gap-8 md:bg-inherit`}>
-        <a href="#">Inicio</a>
-        <a href="#">Acerca de mi</a>
-        <a href="#">Mis Proyectos</a>
-        <a href="#">Contacto</a>
+      <nav className={`absolute top-full ${isShowNav ? "left-0" : "-left-full"} w-full grid text-lg px-2 bg-gray-600 duration-200 md:static md:flex md:w-auto md:gap-4 md:bg-inherit`}>
+        <a className="hover:bg-violet-800 p-4 duration-200" href="#">Inicio</a>
+        <a className="hover:bg-violet-800 p-4 duration-200 " href="#">Acerca de mi</a>
+        <a className="hover:bg-violet-800 p-4 duration-200 " href="#">Mis Proyectos</a>
+        <a className="hover:bg-violet-800 p-4 duration-200 " href="#">Contacto</a>
       </nav>
     </header>
   );
