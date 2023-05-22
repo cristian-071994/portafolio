799047f6f2b6ee77731ffd5f14ab31e70e14caee
Cristian Gutierrez <cristian07199420@gmail.com>
2023-05-22 18:06:35 -0500

    avance my projects responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,11 +9,11 @@ const NavBar = () => {
     }
 
   return (
-    <header className="flex justify-between py-8 px-5 text-2xl text-white items-center bg-[#1A0B2E] relative md:py-0 z-50">
+    <header className="flex justify-between py-8 px-4 text-2xl text-white items-center bg-[#1A0B2E] relative md:py-0 z-50">
       <h1 className="font-semibold">Cristian.G</h1>
       <i onClick={handleShowNav} className="bx bx-menu text-3xl cursor-pointer md:hidden"></i>
 
-      <nav className={`absolute top-full ${isShowNav ? "left-0" : "-left-full"} w-full grid text-lg bg-indigo-950 duration-200 md:static md:flex md:w-auto md:gap-4 md:bg-inherit`}>
+      <nav className={`absolute top-full ${isShowNav ? "left-0" : "-left-full"} w-full grid text-lg bg-indigo-950 duration-200 md:static md:flex md:w-auto md:gap-2 md:bg-inherit md:text-center md:items-center`}>
         <a className="hover:bg-violet-800 p-4 duration-200 pl-2" href="#">Inicio</a>
         <a className="hover:bg-violet-800 p-4 duration-200 pl-2" href="#">Acerca de mi</a>
         <a className="hover:bg-violet-800 p-4 duration-200 pl-2" href="#">Mis habilidades</a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,14 +9,19 @@ const Projects = () => {
     const handleClickProjectPokedex = () => {
         window.open('https://pokedex-by-cgutierrez.netlify.app/', '_blank')
     }
+
+    const handleClickProjectCrud = () => {
+        window.open('https://base-usuarios-by-cg.netlify.app/', '_blank')
+    }
+    
   return (
     <article className='mt-5'>
         <h2 className='text-center text-4xl font-extrabold'>Mis proyectos</h2>
-        <article className='max-w-[470px] p-2 mx-auto'>
-            <h2>Pokedex</h2>
-            <section className='grid gap-2 py-2'>
+        <article className='max-w-[470px] p-2 mx-auto md:w-full md:max-w-[960px] md:p-6'>
+            <h2 className='font-bold text-[#7127BA]'>Pokedex</h2>
+            <section className='grid gap-2 py-2 md:flex md:gap-4 md:items-center'>
                 <div className='max-w-full rounded-md border-[2px] border-[#38126D]'>
-                    <img className='object-cover' src="/images/pokedex.png" alt="" />
+                    <img className='object-cover rounded-md md:object-contain' src="/images/pokedex.png" alt="" />
                 </div>
                 
                     <p>
@@ -24,13 +29,27 @@ const Projects = () => {
                     </p>
                 
             </section>
-            <button className='grid w-auto mx-auto p-2 border-[2px] rounded-md' onClick={handleClickProjectPokedex}>Ver proyecto</button>
+            <button className='grid w-auto mx-auto p-2 border-[2px] border-[#7127BA] rounded-md' onClick={handleClickProjectPokedex}>Ver proyecto</button>
         </article>
-        <article className='max-w-[470px] p-2 mx-auto'>
-            <h2>Rick and Morty</h2>
-            <section className='grid gap-2 py-2'>
+        <article className='max-w-[470px] p-2 mx-auto md:w-full md:max-w-[960px] md:p-6'>
+            <h2 className='font-bold text-[#7127BA]'>Metods CRUD - Api</h2>
+            <section className='grid gap-2 py-2 md:flex md:gap-4 md:items-center'>
                 <div className='max-w-full rounded-md border-[2px] border-[#38126D]'>
-                    <img className='object-cover' src="/images/rick-and-morty.png" alt="" />
+                    <img className='object-cover rounded-md md:object-contain' src="/images/crud.png" alt="" />
+                </div>
+                
+                    <p>
+                    Crea, actualiza y elimina información en este prototipo de creación de usuarios amigable para ti con un diseño elegante.
+                    </p>
+                
+            </section>
+            <button className='grid w-auto mx-auto p-2 border-[2px] border-[#7127BA] rounded-md' onClick={handleClickProjectCrud}>Ver proyecto</button>
+        </article>
+        <article className='max-w-[470px] p-2 mx-auto md:w-full md:max-w-[960px] md:p-6'>
+            <h2 className='font-bold text-[#7127BA]'>Rick and Morty</h2>
+            <section className='grid gap-2 py-2 md:flex md:gap-4 md:items-center'>
+                <div className='max-w-full rounded-md border-[2px] border-[#38126D]'>
+                    <img className='object-cover rounded-md md:object-contain' src="/images/rick-and-morty.png" alt="" />
                 </div>
                 
                     <p>
@@ -38,7 +57,7 @@ const Projects = () => {
                     </p>
                 
             </section>
-            <button className='grid w-auto mx-auto p-2 border-[2px] rounded-md' onClick={handleClickProjectRickandMorty}>Ver proyecto</button>
+            <button className='grid w-auto mx-auto p-2 border-[2px] border-[#7127BA] rounded-md' onClick={handleClickProjectRickandMorty}>Ver proyecto</button>
         </article>
     </article>
   )
